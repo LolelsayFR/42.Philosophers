482b55fc176521e2bea5949f27720e2a220dce3f
Emilien Maillet <emaillet@c2r7p5.42lehavre.fr>
2025-03-20 00:58:42 +0100

    Fix Exit forbidden function

--- philo/script/main.c
+++ philo/script/main.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 00:08:25 by emaillet          #+#    #+#             */
-/*   Updated: 2025/03/12 05:39:14 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/03/20 00:56:44 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,34 +54,39 @@ void	data_free(t_philo_data *data, t_list *philo)
 	ft_lstclear(ft_alist(), free);
 }
 
-static void	data_checker(t_philo_data *data)
+static int	data_checker(t_philo_data *data, char **av)
 {
+	if (av[5] != NULL && av[5][0] != '\0' && ft_atol(av[5]) > 0
+		&& ft_atol(av[5]) < LONG_MAX)
+		data->n_must_eat = ft_atol(av[5]);
+	else
+		data->n_must_eat = -1;
 	if (data->philo_c <= 0 || data->philo_c > 200)
-		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_IPA), exit (-1));
+		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_IPA), -1);
 	if (data->ttdie <= 0 || data->ttdie > INT_MAX)
-		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTD), exit (-1));
+		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTD), -1);
 	if (data->tteat <= 0 || data->tteat > INT_MAX)
-		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTE), exit (-1));
+		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTE), -1);
 	if (data->ttsleep <= 0 || data->ttsleep > INT_MAX)
-		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTS), exit (-1));
+		return (ft_lstclear(ft_alist(), free), wr_error(LANG_E_TTS), -1);
+	return (1);
 }
 
-static void	data_init(t_philo_data *data, char **av, t_list **philo)
+static int	data_init(t_philo_data *data, char **av, t_list **philo)
 {
 	static long	i = 0;
 
+	pthread_mutex_init(data->shield, NULL);
+	pthread_mutex_init(data->philo_edit, NULL);
+	pthread_mutex_init(data->wr_msg, NULL);
 	pthread_mutex_lock(data->philo_edit);
 	data->philo_c = ft_atol(av[1]);
 	data->fork_c = data->philo_c;
 	data->ttdie = ft_atol(av[2]);
 	data->tteat = ft_atol(av[3]);
 	data->ttsleep = ft_atol(av[4]);
-	if (av[5] != NULL && av[5][0] != '\0' && ft_atol(av[5]) > 0
-		&& ft_atol(av[5]) < LONG_MAX)
-		data->n_must_eat = ft_atol(av[5]);
-	else
-		data->n_must_eat = -1;
-	data_checker(data);
+	if (data_checker(data, av) == -1)
+		return (-1);
 	while (++i <= data->fork_c)
 		ft_lstadd_front(philo, ft_lstnew(new_philo(data, *philo)));
 	if (data->fork_c > 1)
@@ -92,6 +97,7 @@ static void	data_init(t_philo_data *data, char **av, t_list **philo)
 	pthread_mutex_unlock(data->philo_edit);
 	while (philo_lstiter_end(*philo, data))
 		philo_lstiter_end(*philo, data);
+	return (1);
 }
 
 int	main(int ac, char **av)
@@ -109,14 +115,12 @@ int	main(int ac, char **av)
 		ft_lstnew(data->shield = ft_calloc(1, sizeof(pthread_mutex_t))));
 	ft_lstadd_front(ft_alist(),
 		ft_lstnew(data->wr_msg = ft_calloc(1, sizeof(pthread_mutex_t))));
-	pthread_mutex_init(data->shield, NULL);
-	pthread_mutex_init(data->philo_edit, NULL);
-	pthread_mutex_init(data->wr_msg, NULL);
 	philo = NULL;
 	if (data == NULL || data->philo_edit == NULL || data->shield == NULL
 		|| data->wr_msg == NULL)
 		return (wr_error(LANG_E_MALLOC), RETURN_ERROR);
-	data_init(data, av, &philo);
+	if (data_init(data, av, &philo) == -1)
+		return (RETURN_ERROR);
 	if (PHILO_DEBUG)
 		printf(YEL"MAIN LOOP FINISHED"RES);
 	data_free(data, philo);
